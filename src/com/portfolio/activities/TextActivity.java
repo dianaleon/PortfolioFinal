dea2974570fd4b20e57beb2c8f9e688cb3192a6c
dianaleon <dianileon@gmail.com>
2013-11-14 04:54:17 +0800

    merge fixing, vuelta a la version de ale.

--- src/com/portfolio/activities/TextActivity.java
+++ src/com/portfolio/activities/TextActivity.java
@@ -13,40 +13,41 @@ import com.portfolio.components.menu;
 
 public class TextActivity extends Activity {
 
-	private Button buttonMenu;
+        private Button buttonMenu;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		
-		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
-		setContentView(R.layout.activity_text);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                
+                super.onCreate(savedInstanceState);
+                requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+                setContentView(R.layout.activity_text);
         Bundle bundle = this.getIntent().getExtras();
 
-        String text = bundle.getString("text");
+//        String text = bundle.getString("text");
         //TextView textView = (TextView) findViewById(R.id.text);
         //textView.setText(text);
         
         final menu menuLayout = (menu) findViewById(R.id.layout_menu);
+        menuLayout.init();
+        
+        buttonMenu = (Button) findViewById(R.id.buttonMenu);
+                buttonMenu.setOnClickListener(new OnClickListener() {
+                public void onClick(View v) {
+                        if (menuLayout.getVisibility() == View.VISIBLE) {
+                                menuLayout.setVisibility(View.INVISIBLE);
+                        } else {
+                                menuLayout.setVisibility(View.VISIBLE);                                
+                        }
+                }
+            });
 
-		buttonMenu = (Button) findViewById(R.id.buttonMenu);
-		buttonMenu.setOnClickListener(new OnClickListener() {
-	        public void onClick(View v) {
-	        	if (menuLayout.getVisibility() == View.VISIBLE) {
-	        		menuLayout.setVisibility(View.INVISIBLE);
-	        	} else {
-	        		menuLayout.setVisibility(View.VISIBLE);	        		
-	        	}
-	        }
-	    });
+        }
 
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+                // Inflate the menu; this adds items to the action bar if it is present.
+                getMenuInflater().inflate(R.menu.main, menu);
+                return true;
+        }
 
 }
--- src/com/portfolio/components/menu.java
+++ src/com/portfolio/components/menu.java
@@ -1,40 +1,91 @@
 package com.portfolio.components;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.portfolio.R;
+import com.portfolio.activities.ContactActivity;
+import com.portfolio.activities.NetworkActivity;
+import com.portfolio.activities.PhotoTextListActivity;
+import com.portfolio.activities.PhotoTittleTextActivity;
 import com.portfolio.activities.TextActivity;
 
 public class menu extends LinearLayout {
-	
-	//private TextView textPageItem;
-	//private TextView galleryPageItem;
-	//private TextView contactPageItem;
-	//private TextView socialPageItem;
-	private Button buttonItem1;
-	private Button buttonItem2;
-	private Button buttonItem3;
-	private Button buttonItem4;
-	private Button buttonItem5;
-	private Button buttonItem6;
-	private LinearLayout menu;
-	private TextActivity ta;
-	
-	public menu(Context context) {
-		super(context);
-		
-	}
+        
+        //private TextView textPageItem;
+        //private TextView galleryPageItem;
+        //private TextView contactPageItem;
+        //private TextView socialPageItem;
+        private Button buttonItem1;
+        private Button buttonItem2;
+        private Button buttonItem3;
+        private Button buttonItem4;
+        private Button buttonItem5;
+        private Button buttonItem6;
+        private LinearLayout menu;
+        private TextActivity ta;
+        
+        public menu(Context context) {
+                super(context);
+                init();
+        }
 
-	public menu(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
-	
-	
-	
-
+        public menu(Context context, AttributeSet attrs) {
+                super(context, attrs);
+        }
+        
+    public void init() {
+            buttonItem1 = (Button) findViewById(R.id.itemMenu1);
+            buttonItem1.setOnClickListener(new OnClickListener() {
+                        
+                        @Override
+                        public void onClick(View v) {
+                                getContext().startActivity(new Intent(getContext(), TextActivity.class));
+                        }
+                });
+            buttonItem2 = (Button) findViewById(R.id.itemMenu2);
+            buttonItem2.setOnClickListener(new OnClickListener() {
+                        
+                        @Override
+                        public void onClick(View v) {
+                                getContext().startActivity(new Intent(getContext(), ContactActivity.class));
+                        }
+                });
+            buttonItem3 = (Button) findViewById(R.id.itemMenu3);
+            buttonItem3.setOnClickListener(new OnClickListener() {
+                        
+                        @Override
+                        public void onClick(View v) {
+                                getContext().startActivity(new Intent(getContext(), NetworkActivity.class));
+                        }
+                });
+            buttonItem4 = (Button) findViewById(R.id.itemMenu4);
+            buttonItem4.setOnClickListener(new OnClickListener() {
+                        
+                        @Override
+                        public void onClick(View v) {
+                                getContext().startActivity(new Intent(getContext(), PhotoTittleTextActivity.class));
+                        }
+                });
+            buttonItem5 = (Button) findViewById(R.id.itemMenu4);
+            buttonItem5.setOnClickListener(new OnClickListener() {
+                        
+                        @Override
+                        public void onClick(View v) {
+                                getContext().startActivity(new Intent(getContext(), PhotoTextListActivity.class));
+                        }
+                });
+            buttonItem6 = (Button) findViewById(R.id.itemMenu4);
+            buttonItem6.setOnClickListener(new OnClickListener() {
+                        
+                        @Override
+                        public void onClick(View v) {
+                                getContext().startActivity(new Intent(getContext(), TextActivity.class));
+                        }
+                });
+    }
 }
-	
-	
-
